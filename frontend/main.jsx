21843001ed9c7bd76991e01edcc2106c6eaa3407
keiji.yatsurugi <keiji.yatsurugi@tradewaltz.com>
2025-01-03 18:13:48 +0900

    Initial commit

---
+++ frontend/main.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/`)
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>API Test</h1>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </div>
+  );
+
+  {/* 
+  return <h1>Hello React + Vite</h1>
+  */}
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
